e0d248054836da6197414fb492304202d616cc11
sajidkhan100 <khsajidkhan123@gmail.com>
2025-01-31 23:42:02 +0500

    Initial commit

---
+++ bank/management/system/Conn.java
@@ -0,0 +1,20 @@
+
+package bank.management.system;
+
+import java.sql.*;
+
+public class Conn {
+  
+    Connection c;
+    Statement s;
+    public Conn(){
+        try {
+            
+            c = DriverManager.getConnection("jdbc:mysql://localhost/bankmanagementsystem", "root", "12345678");
+            s = c.createStatement();
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
